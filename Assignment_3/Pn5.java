b8e803f96856cbbf3cd8a9ae61db075b67373b8a
SwapnaliDeshmukh <101039198+SwapnaliDeshmukh@users.noreply.github.com>
2022-03-23 15:17:01 +0530

    Add files via upload

---
+++ Assignment_3/Pn5.java
@@ -0,0 +1,18 @@
+class Pn5
+{ 
+	public static void main(String args[])
+	{  
+	    char a=65;
+		for(int i=1;i<=5;i++)
+		{ 
+		    for(k=1;k<=5
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+				
+			}
+				System.out.println();
+		     a++;
+		}
+	}
+}	
